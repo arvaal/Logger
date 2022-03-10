08df5660f060ed2b09c467cd4c53a18b7996124a
vagan <vagan@vagan-pc>
2022-03-10 15:16:51 +0300

    litle fix

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 
-spl_autoload_register(function($class_name) {
-    
+spl_autoload_register(function ($class_name) {
+
     $search = ['\\'];
     $replace = ['/'];
-    
+
     $path = str_replace($search, $replace, $class_name . '.php');
-    
+
     $path_adapter = explode('/', $path);
     unset($path_adapter[0]);
-    
+
     $path = implode('/', $path_adapter);
-    
+
     require __DIR__ . '/' . $path;
 });
 
